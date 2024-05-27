fd71419ff68f079cced16eeec54635d3d870f12f
Kirito-nikisa <nikosa090@gmail.com.com>
2024-05-27 20:19:05 +0300

    Added hom ex1

--- main.cpp
+++ main.cpp
@@ -2,6 +2,28 @@
 
 using namespace std;
 
-int main() {
+double power(double base, int exponent) {
+	double result = 1.0;
+	if (exponent < 0) {
+		base = 1.0 / base;
+		exponent = -exponent;
+	}
+	for (int i = 0; i < exponent; ++i)
+	{
+		result *= base;
+	}
+	return result;
+}
 
+int main() {
+	double base;
+	int exponent;
+	cout << "Enter base: ";
+	cin >> base;
+	cout << "Enter exponent: ";
+	cin >> exponent;
+
+	double result = power(base, exponent);
+	cout << base << " raised of the power of " << exponent << " is " << result << endl;
+	return 0;
 }
